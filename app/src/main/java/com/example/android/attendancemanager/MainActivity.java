6a6a0d8f277f1527c7fbe35f108d19b391a91b0d
sanchit <Sharmasanchit1310@gmail.com>
2019-03-09 12:59:52 +0530

    connected to firebase

--- app/src/main/java/com/example/android/attendancemanager/MainActivity.java
+++ app/src/main/java/com/example/android/attendancemanager/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.android.attendancemanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    private TextView email;
+    private TextView password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        email = (TextView)
+
+        mAuth.createUserWithEmailAndPassword()
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
     }
 }
