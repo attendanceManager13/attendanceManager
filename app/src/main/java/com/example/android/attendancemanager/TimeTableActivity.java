dfc1f789383ebec3e6f621bb81757a5b488020df
sanchit <Sharmasanchit1310@gmail.com>
2019-03-10 21:02:10 +0530

    added some funtionalities of subjects and timetable activities from navigation drawer

---
+++ app/src/main/java/com/example/android/attendancemanager/TimeTableActivity.java
@@ -0,0 +1,24 @@
+package com.example.android.attendancemanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class TimeTableActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_table);
+
+        recyclerView = findViewById(R.id.my_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        //mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
+    }
+}
